fe4bcd51e74532ba89ea3df6f76358546a47723e
patangeamruta <30772211+patangeamruta@users.noreply.github.com>
2017-08-10 16:37:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,14 @@
+
+
 console.log('Loaded!');
+
+//change the text
 var element=document.getElementById('main');
 element.innerHTML='new value1';
+
+//move the image
+var img=document.getElementById('madi');
+img.onclick=function(){
+    
+  img.style.maginleft='100px';  
+};
