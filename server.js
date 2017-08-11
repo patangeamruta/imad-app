60766ee8c19b64bbbdc13e8a9d4de2b6e1669147
patangeamruta <30772211+patangeamruta@users.noreply.github.com>
2017-08-11 15:17:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,6 +85,13 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/:articleName', function (req, res) {
+    //articleName==article-one
+    //articles[articleName]==article-one
+    var articleName=req.params.articleName
+  res.send(Createtemplate(articles[articleName]));
+});
+
 var counter=0;
 app.get("/counter",function(req,res){
     counter=counter+1;
@@ -92,12 +99,7 @@ app.get("/counter",function(req,res){
     
 });
 
-app.get('/:articleName', function (req, res) {
-    //articleName==article-one
-    //articles[articleName]==article-one
-    var articleName=req.params.articleName
-  res.send(Createtemplate(articles[articleName]));
-});
+
 
 
 
