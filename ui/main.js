c9dda5346363002086d1872022e5f0912b0f8200
patangeamruta <30772211+patangeamruta@users.noreply.github.com>
2017-08-13 18:05:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,25 +27,18 @@ request.open('GET','http://patangeamruta16.imad.hasura-app.io/counter',true);
 
 //submit name
 
-
+var nameinput=document.getElementById('name');
+var name=nameinput.value;
 var submit=document.getElementById('submit_btn');
 
 
 submit.onclick=function()
 {
-  //make the request to the server and send the name
-   //Create a request object
-  var request=new XMLHttpRequest();
   
-  //capture the response and store it in variable
-  request.onreadystatechange=function(){
-    if(request.readystate===XMLHttpRequest.DONE){
-        //Take some Action
-        if(request.status===200){
             //Capture a list of name and render as list.
-              
-             var names=request.responseText;
-             names = JSON.parse(names);
+              var names=['name1','name2'];
+            // var names=request.responseText;
+             //names = JSON.parse(names);
               var list='';
               for(var i=0;i<names.length;i++)
               {
@@ -54,15 +47,7 @@ submit.onclick=function()
               var ul=document.getElementById('namelist');
               ul.innerHTML = list;
             
-        }
-    } 
+         
     
-  };
-  
-var nameinput=document.getElementById('name');
-var name=nameinput.value;
-//Make a request
-request.open('GET','http://patangeamruta16.imad.hasura-app.io/submit-name?name=' +name,true);
- request.send(null);
   
 };
