510351542a63cffcec473c11ffc5549058d24fc2
patangeamruta <30772211+patangeamruta@users.noreply.github.com>
2017-08-13 17:57:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -93,9 +93,9 @@ app.get("/counter",function(req,res){
 });
 
 
-
+var names=[];
 app.get('/submit-name',function(req,res){  //URL /submit-name?name=xxxx
-  var names=[];
+  
    var name=req.query.name;
 
    names.push(name);
