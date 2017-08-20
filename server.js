c93b64f90f6c2f074f4c0396c39e31e3eb733b10
patangeamruta <30772211+patangeamruta@users.noreply.github.com>
2017-08-20 12:22:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pool=require('pg').pool;
+
+var config={
+user:'patangeamruta16',
+database:'patangeamruta16',
+host:'db.imad.hasura-app.io',
+port:'5432',
+password:process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -81,8 +90,23 @@ var htmlTemplate=`
 `;
   return htmlTemplate;
  }
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var pool=new pool(config);
+app.get('/test-db',function(req,res){
+   //make a select request 
+   //get the response with the results
+   pool.query('SELECT * FROM test', function(err, result){
+       if(err){
+           res.status(500).send(err.toString());
+       }
+       else{
+          res.send(JSON.stringify(result)); 
+       }
+   });
 });
 
 var counter=0;
