6559d20188f19ccc7c3abf62144842f812b21d24
patangeamruta <30772211+patangeamruta@users.noreply.github.com>
2017-08-20 14:40:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -127,11 +127,32 @@ app.get('/submit-name',function(req,res){  //URL /submit-name?name=xxxx
    res.send(JSON.stringify(names));
 });
 
-app.get('/:articleName', function (req, res) {
+/*app.get('/:articleName', function (req, res) {
     //articleName==article-one
     //articles[articleName]==article-one
     var articleName=req.params.articleName;
   res.send(Createtemplate(articles[articleName]));
+});*/
+
+app.get('/articles/:articleName', function (req, res) {
+    //articleName==article-one
+    //articles[articleName]==article-one
+   
+    pool.query("SELECT * from article where title= "+req.params.articleName,function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+        }
+        else{
+            if(result.rows.length===0){
+                res.status(404).send('Article not found');
+            }
+            else{
+                var articleData=result.rows(0);
+                res.send(Createtemplate(articles[articleData]));
+            }
+        }
+    });
+  res.send(Createtemplate(articles[articleData]));
 });
 
 
