53452fc0214baee628a504481861d111292c41aa
patangeamruta <30772211+patangeamruta@users.noreply.github.com>
2017-08-20 14:48:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -138,7 +138,7 @@ app.get('/articles/:articleName', function (req, res) {
     //articleName==article-one
     //articles[articleName]==article-one
    
-    pool.query("SELECT * from article where title= "+req.params.articleName,function(err,result){
+    pool.query("SELECT * from article where title= '"+req.params.articleName+"'",function(err,result){
         if(err){
             res.status(500).send(err.toString());
         }
