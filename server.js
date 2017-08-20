1a65ff67e633e313e7e90d9c8c0ee5255c412831
patangeamruta <30772211+patangeamruta@users.noreply.github.com>
2017-08-20 14:55:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -147,7 +147,7 @@ app.get('/articles/:articleName', function (req, res) {
                 res.status(404).send('Article not found');
             }
             else{
-                var articleData=result.rows(0);
+                var articleData=result.rows[0];
                 res.send(Createtemplate(articleData));
             }
         }
